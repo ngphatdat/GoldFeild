b57a93612e7b25a08f8f4a52e5e6953c23b6caa7
ngphatdat <147393737+ngphatdat@users.noreply.github.com>
2024-08-05 21:17:55 +0700

    fistcode

---
+++ cagefs/domains/goldfield.net.vn/public_html/connectdb.php
@@ -0,0 +1,6 @@
+<?php
+    $conn=mysqli_connect("localhost","tfeqrnuy_goldfiel","x5NbtpYdhftKFD4E7LRn","tfeqrnuy_goldfiel")
+    or die ("Không kết nối được database vui lòng kết nối lại!!!");
+    $conn->query("SET NAMES 'utf8mb4'"); 
+    session_start();
+?>
